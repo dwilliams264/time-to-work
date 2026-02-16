b0f2e7fac5a1b23d3603f054ab57f166ba8c371e
danielwilliams <daniel.williams@gymshark.com>
2026-02-16 13:28:19 +0000

    feat: revert app name change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './app.css';
+import './App.css';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import DayCalendar from './components/day-calendar/day-calendar.component';
